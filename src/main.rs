985d22de96a8354d27910f9abace43c429bdfb9c
DhananjayPurohit <dhananjaypurohit7@gmail.com>
2026-06-07 11:03:03 +0000

    fix: update PaymentHash import path for lightning crate

--- src/main.rs
+++ src/main.rs
@@ -258,7 +258,7 @@ mod tests {
     use rocket::local::asynchronous::Client;
     use rocket::serde::json::Value;
     use super::rocket;
-    use lightning::ln::PaymentHash;
+    use lightning::types::payment::PaymentHash;
 
     use l402_middleware::{l402, utils};
 
